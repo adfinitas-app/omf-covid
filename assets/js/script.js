3229f39a4bb060c5e6582a65ff36bd3ef9d3f5f2
thibaut trouve <thibaut.trouve@epitech.eu>
2020-05-05 00:05:14 -0700

    solimalte v1 sans lien

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-const arrowElem = document.getElementById('arrow-down');
-const contentElem = document.getElementById('click-to-expand');
+const arrowElem = document.getElementById("arrow-down");
+const contentElem = document.getElementById("click-to-expand");
 
-arrowElem.addEventListener('click', function() {
-    this.classList.toggle('active');
-    
-    if (contentElem.style.maxHeight && contentElem.style.maxHeight !== '0px') {
-        contentElem.style.maxHeight = '0px';
-    } else {
-        contentElem.style.maxHeight = '2000px';
-        contentElem.style.height = 'auto';
-    }
-})
+if (arrowElem) {
+	arrowElem.addEventListener("click", function () {
+		this.classList.toggle("active");
+
+		if (contentElem.style.maxHeight && contentElem.style.maxHeight !== "0px") {
+			contentElem.style.maxHeight = "0px";
+		} else {
+			contentElem.style.maxHeight = "2000px";
+			contentElem.style.height = "auto";
+		}
+	});
+}
